24c5969ede80673e838b60f01ac4f245faa439a4
manan-iphone <mananavaiya2006@gmail.com>
2025-09-04 15:42:29 +0530

    Add files via upload

---
+++ comparison.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int  main()
+{
+    int a,b;
+    printf("Enter 2 numbers for comparision");
+    scanf("%d %d",&a,&b);
+    if(a>b)
+        printf("Larger among the 2 is: %d \n Smaller among 2 is: %d \n",a,b);
+       else if(a==b)
+            printf("both are equal %d = %d",a,b);
+    else
+            printf("Larger among the 2 is: %d \n Smaller among 2 is: %d \n",b,a);
+
+}
